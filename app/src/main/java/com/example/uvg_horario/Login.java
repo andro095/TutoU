1dded299f7990a9570d93f628a9ab3fff32cfbe8
andro095 <rod18332@uvg.edu.gt>
2019-03-06 07:24:39 -0600

    Version 0.1.1

--- app/src/main/java/com/example/uvg_horario/Login.java
+++ app/src/main/java/com/example/uvg_horario/Login.java
@@ -1,7 +1,9 @@
 package com.example.uvg_horario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    protected void ayuwoki (View view){
+        Intent intent = new Intent(getApplicationContext(),menu_principal.class);
+        startActivity(intent);
     }
 }
